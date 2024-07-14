78ba90fd161014d9d9be8626bbc75c601bebcd5d
SandipMajithiya12 <sandipmajithiya10@gmail.com>
2024-07-14 15:11:52 +0530

    "dynamic data in home page"

--- apps/pwa/src/pages/home/Home.page.tsx
+++ apps/pwa/src/pages/home/Home.page.tsx
@@ -1,8 +1,13 @@
 import { Flex, Input, Select, Tag, Typography } from "antd";
 import React from "react";
 import Card from "@/components/card";
+import { trpc } from "@/trpc/trpc";
 
 export const Home: React.FC = () => {
+  const allBooks = trpc.getAllBooks.useQuery();
+  const data = allBooks.data;
+  console.log(data);
+
   const onChange = (value: string) => {
     console.log(`selected ${value}`);
   };
@@ -39,136 +44,65 @@ export const Home: React.FC = () => {
           />
         </Flex>
       </div>
-      <div className=" flex gap-4">
-        <Card className=" w-40 flex flex-col">
-          <img
-            src="https://books.google.com/books/content?id=yDB0tAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-            alt="img"
-            className=" mb-2"
-          />
-          <div className=" flex flex-col justify-start">
-            <Typography.Text>Lorem ipsum dolor sit amet.</Typography.Text>
-            <Typography.Text style={{ marginLeft: "0px", fontWeight: "bold" }}>
-              Lorem
-            </Typography.Text>
-          </div>
-        </Card>
-        <Card className=" w-40 flex flex-col">
-          <img
-            src="https://books.google.com/books/content?id=yDB0tAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-            alt="img"
-            className=" mb-2"
-          />
-          <div className=" flex flex-col justify-start">
-            <Typography.Text>Lorem ipsum dolor sit amet.</Typography.Text>
-            <Typography.Text style={{ marginLeft: "0px", fontWeight: "bold" }}>
-              Lorem
-            </Typography.Text>
-          </div>
-        </Card>
-        <Card className=" w-40 flex flex-col">
-          <img
-            src="https://books.google.com/books/content?id=yDB0tAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-            alt="img"
-            className=" mb-2"
-          />
-          <div className=" flex flex-col justify-start">
-            <Typography.Text>Lorem ipsum dolor sit amet.</Typography.Text>
-            <Typography.Text style={{ marginLeft: "0px", fontWeight: "bold" }}>
-              Lorem
-            </Typography.Text>
-          </div>
-        </Card>
-        <Card className=" w-40 flex flex-col">
-          <img
-            src="https://books.google.com/books/content?id=yDB0tAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-            alt="img"
-            className=" mb-2"
-          />
-          <div className=" flex flex-col justify-start">
-            <Typography.Text>Lorem ipsum dolor sit amet.</Typography.Text>
-            <Typography.Text style={{ marginLeft: "0px", fontWeight: "bold" }}>
-              Lorem
-            </Typography.Text>
-          </div>
-        </Card>
-        <Card className=" w-40 flex flex-col">
-          <img
-            src="https://books.google.com/books/content?id=yDB0tAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-            alt="img"
-            className=" mb-2"
-          />
-          <div className=" flex flex-col justify-start">
-            <Typography.Text>Lorem ipsum dolor sit amet.</Typography.Text>
-            <Typography.Text style={{ marginLeft: "0px", fontWeight: "bold" }}>
-              Lorem
-            </Typography.Text>
-          </div>
-        </Card>
+        <h3 className=" ml-10 font-bold">New Arrivals</h3>
+      <div className=" flex justify-center">
+      <div className=" flex gap-4 overflow-x-scroll max-w-6xl max-h-96 ">
+        {data?.map((data)=>(
+            <Card className=" min-w-40 flex flex-col">
+            <img
+              src=
+              {data.image}
+              alt="img"
+              className=" mb-2"
+            />
+            <div className=" flex flex-col justify-start">
+              <Typography.Text>{data.title}</Typography.Text>
+              <Typography.Text style={{ marginLeft: "0px", fontWeight: "bold" }}>
+                {data.author}
+              </Typography.Text>
+            </div>
+          </Card>
+  
+        ))
+        }
+        
+      
       </div>
+      </div>
+      
+
       <div className=" grid grid-cols-2 gap-2 mt-12 ml-5  ">
-        <div className=" grid grid-cols-3  gap-2 m-4 rounded-xl shadow-lg">
-          <div className=" col-span-1 m-2 p-2">
-            <img
-              src="https://books.google.com/books/content?id=yDB0tAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-              alt="img"
-            />
-            <h2 className=" mt-2 font-bold">Lorem, ipsum.</h2>
-          </div>
-          <div className=" col-span-2 m-2 p-2">
-            <h3>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam,
-              molestias quo perspiciatis itaque vitae minus quod iure sed et, ad
-              inventore similique enim accusamus reprehenderit corrupti animi.
-              Ad, esse obcaecati....
-            </h3>
-            <Tag
-              bordered={false}
-              color="success"
-              style={{
-                paddingLeft: "15px",
-                paddingRight: "15px",
-                paddingTop: "10px",
-                paddingBottom: "10px",
-                fontSize: "15px",
-                marginTop: "10px",
-              }}
-            >
-              success
-            </Tag>{" "}
-          </div>
-        </div>
-        <div className=" grid grid-cols-3  gap-2 m-4 rounded-xl shadow-lg">
-          <div className=" col-span-1 m-2 p-2">
-            <img
-              src="https://books.google.com/books/content?id=yDB0tAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
-              alt="img"
-            />
-            <h2 className=" mt-2 font-bold">Lorem, ipsum.</h2>
-          </div>
-          <div className=" col-span-2 m-2 p-2">
-            <h3>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam,
-              molestias quo perspiciatis itaque vitae minus quod iure sed et, ad
-              inventore similique enim accusamus reprehenderit corrupti animi.
-              Ad, esse obcaecati....
-            </h3>
-            <Tag
-              bordered={false}
-              color="success"
-              style={{
-                paddingLeft: "15px",
-                paddingRight: "15px",
-                paddingTop: "10px",
-                paddingBottom: "10px",
-                fontSize: "15px",
-                marginTop: "10px",
-              }}
-            >
-              success
-            </Tag>
-          </div>
-        </div>
+        {data?.map((data)=>(
+           <div className=" grid grid-cols-3  gap-2 m-4 rounded-xl shadow-lg">
+           <div className=" col-span-1 m-2 p-2">
+             <img
+               src={data.image}
+               alt="img"
+             />
+             <h2 className=" mt-2 font-bold">{data.author}</h2>
+           </div>
+           <div className=" col-span-2 m-2 p-2">
+             <h3>
+               {data.description}
+             </h3>
+             <Tag
+               bordered={false}
+               color="success"
+               style={{
+                 paddingLeft: "15px",
+                 paddingRight: "15px",
+                 paddingTop: "10px",
+                 paddingBottom: "10px",
+                 fontSize: "15px",
+                 marginTop: "10px",
+               }}
+             >
+               success
+             </Tag>{" "}
+           </div>
+         </div>
+        ))}
+       
       </div>
     </div>
   );
